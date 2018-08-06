f3eb3c49045db5b39d719ce89377e2292aa325a8
Dũng Nguyễn <nhymxu@users.noreply.github.com>
2018-08-06 11:48:21 +0700

    Install Dnsmasq

--- macOS.sh
+++ macOS.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
+# Dz's Development Machine Setup on macOS
+# Author: Dung Nguyen (nhymxu) <contact@dungnt.net>
+#
 
+
+# Homebrew package manage
+xcode-select --install
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Dnsmasq - simple local dns for development
+brew install dnsmasq
+sudo brew services start dnsmasq
