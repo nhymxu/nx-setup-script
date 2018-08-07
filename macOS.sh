2a8f27f04139e7780864b8f592e0a601014e4fe5
Dũng Nguyễn <nhymxu@users.noreply.github.com>
2018-08-07 20:07:09 +0700

    setup test domain with dnsmasq

--- macOS.sh
+++ macOS.sh
@@ -8,6 +8,15 @@
 xcode-select --install
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+
+
 # Dnsmasq - simple local dns for development
 brew install dnsmasq
-sudo brew services start dnsmasq
+echo -e '# Customize by nhymxu\nconf-dir=/usr/local/etc/dnsmasq.d/,*.conf' >> $(brew --prefix)/etc/dnsmasq.conf
+sudo mkdir -v /etc/resolver
+
+sudo bash -c 'echo "nameserver 127.0.0.1" > /etc/resolver/test'
+echo 'address=/test/127.0.0.1' > $(brew --prefix)/etc/dnsmasq.d/test.conf
+
+sudo launchctl stop homebrew.mxcl.dnsmasq
+sudo launchctl start homebrew.mxcl.dnsmasq
