92cb63d78bee58eae41cc1ece6110a6cdde0c7f3
Dũng Nguyễn <nhymxu@users.noreply.github.com>
2018-09-17 11:38:03 +0700

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -165,3 +165,7 @@ sudo apt install -y net-tools
 wget https://github.com/sharkdp/bat/releases/download/v0.6.1/bat_0.6.1_amd64.deb
 sudo dpkg -i bat_0.6.1_amd64.deb
 rm -rf bat_0.6.1_amd64.deb
+
+### Updated motd
+sudo apt install -y update-motd
+
