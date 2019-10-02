02cb77916bfd83b1edd6b953711c282d3c9d32a9
Dung Nguyen <contact@dungnt.net>
2019-10-02 23:02:17 +0700

    fix nginx php socket problem

--- ubuntu-homeserver.sh
+++ ubuntu-homeserver.sh
@@ -41,3 +41,6 @@ sudo apt update
 sudo apt install nginx
 sudo apt install php7.3-fpm php7.3-bcmath php7.3-bz2 php7.3-cli php7.3-curl php7.3-gd php7.3-imap php7.3-intl php7.3-json php7.3-mbstring php7.3-mysql php7.3-pgsql php7.3-redis php7.3-sqlite3 php7.3-xml php7.3-zip php7.3-xsl
 sudo apt install mariadb-server
+
+## Other task
+sudo usermod -aG www-data nginx
