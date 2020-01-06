565f80ec246ecaa5820b37cba966ffb34d9dc317
Dung Nguyen <contact@dungnt.net>
2020-01-06 10:30:30 +0700

    add php gmp extension

--- ubuntu-homeserver.sh
+++ ubuntu-homeserver.sh
@@ -39,7 +39,7 @@ echo "deb [arch=amd64,arm64,ppc64el] http://sgp1.mirrors.digitalocean.com/mariad
 ## Install 
 sudo apt update
 sudo apt install nginx apache2-utils
-sudo apt install php7.3-fpm php7.3-bcmath php7.3-bz2 php7.3-cli php7.3-curl php7.3-gd php7.3-imap php7.3-intl php7.3-json php7.3-mbstring php7.3-mysql php7.3-pgsql php7.3-redis php7.3-sqlite3 php7.3-xml php7.3-zip php7.3-xsl
+sudo apt install php7.3-fpm php7.3-bcmath php7.3-bz2 php7.3-cli php7.3-curl php7.3-gd php7.3-gmp php7.3-imap php7.3-intl php7.3-json php7.3-mbstring php7.3-mysql php7.3-pgsql php7.3-redis php7.3-sqlite3 php7.3-xml php7.3-zip php7.3-xsl
 sudo apt install mariadb-server
 
 ## Other task
