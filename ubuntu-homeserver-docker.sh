0f3c27f19f76542162698590d51e602f5cd802bb
Dung Nguyen <contact@dungnt.net>
2020-09-22 12:50:27 +0700

    Refactor for easy show config

--- ubuntu-homeserver-docker.sh
+++ ubuntu-homeserver-docker.sh
@@ -5,11 +5,21 @@
 #
 
 # MariaDB
-docker run --name mariadb -p 3306:3306 -v /data/database/mysql:/var/lib/mysql -e MYSQL_RANDOM_ROOT_PASSWORD=yes mariadb
+docker run --name mariadb \
+  -p 3306:3306 \
+  -v /data/database/mysql:/var/lib/mysql \
+  -e MYSQL_RANDOM_ROOT_PASSWORD=yes \
+  -d mariadb
 
 # Clickhouse
-docker run -d --name clickhouse-analytics-server -p 8123:8123 -p 9000:9000 -p 9009:9009  --ulimit nofile=262144:262144 --volume=/data/database/sclickhouse:/var/lib/clickhouse yandex/clickhouse-server
-docker run -d --name clickhouse-analytics-server -p 8123:8123 -p 9000:9000 -p 9009:9009  --ulimit nofile=262144:262144 --volume=/data/database/sclickhouse:/var/lib/clickhouse -v /data/database/clickhouse_config.xml:/etc/clickhouse-server/config.xml yandex/clickhouse-server
+docker run --name clickhouse-analytics-server \
+  -p 8123:8123 \
+  -p 9000:9000 \
+  -p 9009:9009 \
+  --ulimit nofile=262144:262144 \
+  --volume=/data/database/sclickhouse:/var/lib/clickhouse \
+#  -v /data/database/clickhouse_config.xml:/etc/clickhouse-server/config.xml \
+  -d yandex/clickhouse-server
 
 # PostgreSQL
 docker run --name postgres12 \
@@ -20,20 +30,25 @@ docker run --name postgres12 \
   -d postgres:12
 
 # MongoDB
-docker run --name mongodb -p 27017:27017 -v /data/database/mongodb:/data/db -d mongo:4-bionic mongod --replSet nx-repl-set
+docker run --name mongodb \
+  -p 27017:27017 \
+  -v /data/database/mongodb:/data/db \
+  -d mongo:4-bionic \
+  mongod --replSet nx-repl-set
+
 docker exec -it mongodb mongo
 # Run on mongo shell: rs.initiate()
 
 # Bitwarden
 # Gen token using openssl rand -base64 48
-docker run -d --name bitwarden \
+docker run --name bitwarden \
   -e SIGNUPS_ALLOWED=false \
   -e ADMIN_TOKEN=your_admin_token_here \
   -e WEBSOCKET_ENABLED=true \
   -v /data/database/bitwardenrs/:/data/ \
   -p 16000:80 \
   -p 16001:3012 \
-  bitwardenrs/server:testing
+  -d bitwardenrs/server:testing
   
 # Adguard home
 docker run --name adguardhome \
@@ -51,10 +66,10 @@ docker run --name adguardhome \
   -d adguard/adguardhome
 
 # n8n.io - Workflow Automation Tool
-docker run -d \
-	--name n8n \
-	-p 16010:5678 \
-	-e GENERIC_TIMEZONE="Asia/Ho_Chi_Minh" \
-	-e TZ="Asia/Ho_Chi_Minh" \
-	-v /data/database/n8n:/root/.n8n \
-	n8nio/n8n
+docker run \
+  --name n8n \
+  -p 16010:5678 \
+  -e GENERIC_TIMEZONE="Asia/Ho_Chi_Minh" \
+  -e TZ="Asia/Ho_Chi_Minh" \
+  -v /data/database/n8n:/root/.n8n \
+  -d n8nio/n8n
